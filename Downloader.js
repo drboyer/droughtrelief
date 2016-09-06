1189a87237278e625ae486270e00d15a6ef67cf1
Devin Boyer <drb5272@gmail.com>
2016-09-05 21:25:21 -0400

    Reconfigure app to use Redis for caching

--- Downloader.js
+++ Downloader.js
@@ -7,6 +7,7 @@ var ftp = require('ftp'),
     fs = require('fs'),
     toJSON = require('shp2json'),
     stream = require('stream'),
+    toString = require('stream-to-string'),
     tar = require('tar-fs'),
     Promise = require('bluebird'),
     dates = require('./dates')
@@ -42,14 +43,53 @@ var downloader = class Downloader extends EventEmitter {
         ftp_client.connect({ host: 'ftp.wpc.ncep.noaa.gov' })
     }
 
-    getDroughtMonStream (outStream) {
-        var pass = new stream.PassThrough()
-        var droughtMonDate = dates.getDroughtMonDate()
+    getQPFAsString() {
+        // Get formatted dates
+        var tarDate = dates.getQPFTarDate(),
+            shpDate = dates.getQPFShpDate()
 
-        req('http://droughtmonitor.unl.edu/data/shapefiles_m/USDM_' + droughtMonDate + '_M.zip')
+        var ftp_client = new ftp()
+        Promise.promisifyAll(ftp_client)
+
+        ftp_client.on('ready', () => {
+            ftp_client.getAsync('shapefiles/qpf/5day/95e_' + tarDate + '.tar').then( (ftpStream) => {
+                // if (err) throw err;
+                ftpStream.once('close', () => { ftp_client.end() })
+                ftpStream.pipe(tar.extract('./data'))
+                .on('finish', () => {
+                    console.log("Finished download and extraction")
+                    this.emit('extract')
+
+                    var pass = new stream.PassThrough()
+                    toJSON.fromShpFile('./data/95e' + shpDate + '.shp')
+                        .pipe(pass)
+
+                    toString(pass).then((data) => {
+                        this.emit('qpfJSON-done', data)
+                    })
+                })
+            })
+        })
+
+        ftp_client.connect({ host: 'ftp.wpc.ncep.noaa.gov' })
+    }
+
+    getDroughtMonStream(outStream) {
+        var pass = new stream.PassThrough() 
+
+        req('http://droughtmonitor.unl.edu/data/shapefiles_m/USDM_' + dates.getDroughtMonDate() + '_M.zip')
         .pipe(pass)
 
         toJSON(pass).pipe(outStream)
+    }
+
+    getDroughtMonAsString() {
+        var passStream = new stream.PassThrough()  // pass through stream to go between getDroughtMonStream() and toString()
+        this.getDroughtMonStream(passStream)
+        
+        toString(passStream).then((data) => {
+            this.emit('droughtJSON-done', data)
+        })
     }
 }
 
--- server.js
+++ server.js
@@ -1,23 +1,96 @@
-var express = require('express')
-var morgan = require('morgan')
-var Downloader = require('./Downloader')
+var express = require('express'),
+    morgan = require('morgan'),
+    redis = require('redis')
+    Promise = require('bluebird'),
+    toString = require('stream-to-string')
 
+var Downloader = require('./Downloader'),
+    dates = require('./dates')
+
+// set up application objects
 var app = express()
-var dataDL = new Downloader()
+var dataDownloader = new Downloader()
+// set up redis client
+var redisClient = redis.createClient(process.env.REDIS_URL || 'redis://localhost/')
+Promise.promisifyAll(redis.RedisClient.prototype)
+redisClient.on('error', (err) => {
+    console.log("Redis error: " + err)
+})
 
+// basic web app setup
 app.use(morgan('tiny'))
-// app.use('/static_data', express.static('static_data'))
 app.use(express.static('static_web'))
-
-app.get('/qpf.geojson', (req, res) => {
-    dataDL.getQPFStream(res)
-})
-
-app.get('/drought.geojson', (req, res) => {
-    dataDL.getDroughtMonStream(res)
-})
 
 app.set('port', (process.env.PORT || 8000));
 app.listen(app.get('port'), () => {
     console.log("App started on port", app.get('port'));
 })
+
+/* Route handlers for datasets */
+
+app.get('/qpf.geojson', (req, res) => {
+    var qpf_key = 'qpf_' + dates.getQPFTarDate()
+
+    redisClient.getAsync(qpf_key).then((result) => {
+        if (result == null) {
+            console.log('Downloading new QPF forecast data')
+            dataDownloader.getQPFStream(res)
+            updateRedisQPF()
+        } else {
+            console.log('Getting QPF data from Redis')
+            res.end(result)
+        }
+    })
+})
+
+app.get('/drought.geojson', (req, res) => {
+    var drought_key = 'drought_' + dates.getDroughtMonDate()
+
+    redisClient.getAsync(drought_key).then((result) => {
+        if (result == null) {
+            console.log('Downloading new Drought Monitor data')
+            dataDownloader.getDroughtMonStream(res)
+            updateRedisDrought()
+        } else {
+            console.log('Getting Drought Monitor data from Redis')
+            res.end(result)
+        }
+    })
+})
+
+/* Below are two helper functions used to update Redis with the new data
+   and remove the outdated entries */
+
+function updateRedisQPF() {
+    var qpf_key = 'qpf_' + dates.getQPFTarDate()
+
+    redisClient.keysAsync('qpf_*').then((replyKeys) => {
+        replyKeys.forEach((replyKey, i) => {
+            redisClient.delAsync(replyKey)
+        })
+    }).then( () => {
+        dataDownloader.getQPFAsString()
+        dataDownloader.on('qpfJSON-done', (data) => {
+            redisClient.setAsync(qpf_key, data).then(() => {
+                console.log('Added QPF data to Redis. Key: ' + qpf_key)
+            })
+        })
+    })
+}
+
+function updateRedisDrought() {
+    var drought_key = 'drought_' + dates.getDroughtMonDate()
+
+    redisClient.keysAsync('drought_*').then((replyKeys) => {
+        replyKeys.forEach((replyKey, i) => {
+            redisClient.delAsync(replyKey)
+        })
+    }).then( () => {
+        dataDownloader.getDroughtMonAsString()
+        dataDownloader.on('droughtJSON-done', (data) => {
+            redisClient.setAsync(drought_key, data).then(() => {
+                console.log('Added QPF data to Redis. Key: ' + drought_key)
+            })
+        })
+    })
+}
